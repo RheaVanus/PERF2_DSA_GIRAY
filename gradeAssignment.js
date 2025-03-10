98634dd0313c7c46b672bba3747dbc3fe00df0c2
Jerome Giray <jeromegiray500@gmail.com>
2025-03-10 23:32:56 +0800

    Completed IfElse Section

--- gradeAssignment.js
+++ gradeAssignment.js
@@ -1,5 +1,19 @@
 /*
-If Else If Activity:
+If Else If Activity
+Instructions:
+    1. Create a new JavaScript file named gradeAssignment.js.
+    2. In the file, perform the following:
+        o Create a program that prompts the user to input their score.
+        o Use else if statements to assign grades based on the following criteria:
+            ▪ 90 and above: Excellent
+            ▪ 80 to 89: Good
+            ▪ 70 to 79: Fair
+            ▪ Below 70: Needs Improvement
+            o Log the corresponding grade to the console.
+
+Example Output:
+Enter your score: 85
+Your grade is: Good.
 */
 
 //The grade of student is collected using prompt() before proceeding to evaluate the grade.
